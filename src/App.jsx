af18ed4664a868e497468b2968d601a3bb530049
atenasaritag <atenaslucia23@gmail.com>
2025-04-20 21:22:39 -0600

    fix: added Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Profile from './components/Profile';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Profile />
       <About />
       <Projects />
